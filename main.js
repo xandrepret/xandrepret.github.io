05d222bb9f6b4af4e41b8a18ed653964e42eed37
Unknown <xpretorius@gmail.com>
2018-03-22 14:20:14 +0200

    Bug Fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.onscroll = function() {
 };
 
 // Get the navbar
-var navbar = document.getElementById("navbar");
+var navbar = document.getElementById(navbar);
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
